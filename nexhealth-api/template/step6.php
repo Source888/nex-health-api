4bcad14e5443281c186bfc0453cfefc4db6520ca
dev <dev@orest.usa>
2024-05-15 15:22:37 +0300

    Minor update for prewent null warning when access step befor object created

--- nexhealth-api/template/step6.php
+++ nexhealth-api/template/step6.php
@@ -7,7 +7,7 @@
     <div class = "container">
         <div class = "row">
             <div class = "col-md-12">
-                <h1 class="appt-info-ttl"><?=($body_cont['patient']->first_name)?>, YOU ARE ALL SET</h1>
+                <h1 class="appt-info-ttl"><?=($body_cont['patient']->first_name ?? '')?>, YOU ARE ALL SET</h1>
             </div>
         </div>
         <div class="row">
